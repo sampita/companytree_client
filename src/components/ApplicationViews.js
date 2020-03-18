05e05ae72e9650ee0f61df8dc8c0ba43f6142240
Sam Pita <samanthakpita@gmail.com>
2020-03-18 14:06:00 -0500

    setup AppViews

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Redirect, withRouter } from "react-router-dom";
+
+class ApplicationViews extends Component {
+
+    render() {
+        return (
+            <>
+                {/* <Route exact path="/" render={(props) => {
+                    return <Home 
+                    {...props}
+                    {...this.props} />
+                }} /> */}
+            </>
+        )
+    }
+}
