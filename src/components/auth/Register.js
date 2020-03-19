8270f941e46b58eebdae27a580be78e079d02cd6
Sam Pita <samanthakpita@gmail.com>
2020-03-19 14:05:22 -0500

    working on Register component

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
+import { Button, Form, Dropdown } from 'semantic-ui-react'
 import { withRouter } from "react-router-dom"
-import CompanySearch from "../search/CompanySearch"
+import APIManager from "../../modules/APIManager"
+// import CompanySearch from "../search/CompanySearch"
 
 class Register extends Component {
 
     state = {
+        company: "",
         email: "",
         userName: "",
         firstName: "",
@@ -17,12 +20,22 @@ class Register extends Component {
 
     handleInputChange = (evt) => {
         let stateToChange = {}
+        stateToChange["company"] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    handleInputChange2 = (evt) => {
+        let stateToChange = {}
         stateToChange[evt.target.id] = evt.target.value
         this.setState(stateToChange)
     }
 
-    handleRegister = event => {
-        event.preventDefault()
+    handleChange = (evt) => {
+        this.setState({ value: evt.target.value });
+    }
+
+    handleRegister = (evt) => {
+        evt.preventDefault()
 
         // Creates object with values from state
         const newUser = {
@@ -31,7 +44,8 @@ class Register extends Component {
             "last_name": this.state.lastName,
             "address": this.state.address,
             "email": this.state.email,
-            "password": this.state.password
+            "password": this.state.password,
+            "company": this.state.company
         }
 
         // Makes a fetch call with newUser object as the body of the POST request
@@ -40,85 +54,116 @@ class Register extends Component {
             .then(() => this.props.history.push("/"))
     }
 
+    componentDidMount() {
+        APIManager.getAllNoAuth("companies").then(response => {
+            const newCompanies = [];
+            for (const item of response) {
+                let newItem = { key: `${item.id}`, text: `${item.name}` }
+                newCompanies.push(newItem);
+            }
+            const uniqueCompanies = [...new Set(newCompanies)]
+            this.setState({
+                companies: uniqueCompanies
+            });
+        });
+    }
     render() {
+        console.log("this.state", this.state)
         return (
             <>
-                <section>
-                    <label>Find Your Company:</label>
-                    <CompanySearch/>
-                </section>
-                <br />
-                <form onSubmit={this.handleRegister}>
-                    <h1>Sign Up</h1>
-                    <fieldset>
-                        <input onChange={(evt) => this.handleInputChange(evt)}
-                            id="userName"
-                            type="text"
-                            name="userName"
-                            className="form-control"
-                            placeholder="Username"
-                            required autoFocus />
-                    </fieldset>
-                    <fieldset>
-                        <input onChange={this.handleInputChange}
-                            id="firstName"
-                            type="text"
-                            name="firstName"
-                            className="form-control"
-                            placeholder="First name"
-                            required autoFocus />
-                    </fieldset>
-                    <fieldset>
-                        <input onChange={this.handleInputChange}
-                            id="lastName"
-                            type="text"
-                            name="lastName"
-                            className="form-control"
-                            placeholder="Last name"
-                            required />
-                    </fieldset>
-                    <fieldset>
-                        <input onChange={this.handleInputChange}
-                            id="address"
-                            type="text"
-                            name="address"
-                            className="form-control"
-                            placeholder="Location"
-                            required />
-                    </fieldset>
-                    <fieldset>
-                        <input onChange={this.handleInputChange}
-                            id="email"
-                            type="email"
-                            name="email"
-                            className="form-control"
-                            placeholder="Email address"
-                            required />
-                    </fieldset>
-                    <fieldset>
-                        <input onChange={this.handleInputChange}
-                            id="password"
-                            type="password"
-                            name="password"
-                            className="form-control"
-                            placeholder="Password"
-                            required />
-                    </fieldset>
-                    <fieldset>
-                        <input onChange={this.handleInputChange}
-                            id="verifyPassword"
-                            type="password"
-                            name="verifyPassword"
-                            className="form-control"
-                            placeholder="Verify password"
-                            required />
-                    </fieldset>
-                    <fieldset>
-                        <button type="submit">
-                            Register
-                        </button>
-                    </fieldset>
-                </form>
+                <Form onSubmit={this.handleRegister}>
+                    <section>
+                        <h3>Step 1: Find Your Company:</h3>
+                        <Dropdown
+                            id="company"
+                            placeholder='Select Company'
+                            fluid
+                            search
+                            selection
+                            options={this.state.companies}
+                            onChange={(evt) => this.handleChange}
+                            value={this.state.value}
+                        />
+                        <h4>Don't see your company listed?</h4>
+                        <Button>Register Your Company</Button>
+                        {/* <CompanySearch
+                            handleInputChange={this.handleInputChange}
+                            handleChange={this.handleChange}
+                        /> */}
+                    </section>
+                    <br />
+                    <section>
+                        <h3>Step 2: Create Your Administrator Account</h3>
+                        <Form.Field>
+                            <input onChange={(evt) => this.handleInputChange(evt)}
+                                id="userName"
+                                type="text"
+                                name="userName"
+                                className="form-control"
+                                placeholder="Username"
+                                required autoFocus />
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.handleInputChange}
+                                id="firstName"
+                                type="text"
+                                name="firstName"
+                                className="form-control"
+                                placeholder="First name"
+                                required />
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.handleInputChange}
+                                id="lastName"
+                                type="text"
+                                name="lastName"
+                                className="form-control"
+                                placeholder="Last name"
+                                required />
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.handleInputChange}
+                                id="address"
+                                type="text"
+                                name="address"
+                                className="form-control"
+                                placeholder="Location"
+                                required />
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.handleInputChange}
+                                id="email"
+                                type="email"
+                                name="email"
+                                className="form-control"
+                                placeholder="Email address"
+                                required />
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.handleInputChange}
+                                id="password"
+                                type="password"
+                                name="password"
+                                className="form-control"
+                                placeholder="Password"
+                                required />
+                        </Form.Field>
+                        <Form.Field>
+                            <input onChange={this.handleInputChange}
+                                id="verifyPassword"
+                                type="password"
+                                name="verifyPassword"
+                                className="form-control"
+                                placeholder="Verify password"
+                                required />
+                        </Form.Field>
+                        <Form.Field>
+                            <Button type="submit">
+                                Create Account
+                                </Button>
+                        </Form.Field>
+                    </section>
+                </Form>
             </>
         )
     }
